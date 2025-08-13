1ebf68fbd9d18e0f42a06319e10189101e1c8e77
Lindiwe Thabsile Dlomo <lindiwethaby96@gmail.com>
2025-08-13 09:29:13 +0200

    Update ApiClient.cs

--- LaserSuburbLookup/Services/ApiClient.cs
+++ LaserSuburbLookup/Services/ApiClient.cs
@@ -51,7 +51,7 @@ namespace LaserSuburbLookup.Services
             }
             catch
             {
-                // Not JSON
+             
             }
 
             return content.Trim();
